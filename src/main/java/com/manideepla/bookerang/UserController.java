d882699dadbc748a2e7a9eb82b66039bb246b315
manideepla <manideepla.me@gmail.com>
2024-11-18 12:42:16 -0500

    feat - add base path to rest controller

--- src/main/java/com/manideepla/bookerang/UserController.java
+++ src/main/java/com/manideepla/bookerang/UserController.java
@@ -2,12 +2,14 @@ package com.manideepla.bookerang;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     
-    @PostMapping("/user")
+    @PostMapping("/signup")
     void signupUser(@RequestBody User user) {
         System.out.println(user);
     }
