59c839d28a2d9cb01f75cbdefc84bf9dfe7d65b8
manideepla <manideepla.me@gmail.com>
2025-01-25 21:24:59 -0500

    feat - sample log during startup

--- src/main/java/com/manideepla/bookerang/BookerangApplication.java
+++ src/main/java/com/manideepla/bookerang/BookerangApplication.java
@@ -1,13 +1,18 @@
 package com.manideepla.bookerang;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookerangApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookerangApplication.class);
+
 	public static void main(String[] args) {
+		log.info("Starting BookerangApplication");
 		SpringApplication.run(BookerangApplication.class, args);
 	}
 
