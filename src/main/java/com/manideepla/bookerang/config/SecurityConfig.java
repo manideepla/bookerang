18fdd142cb52246c0cf98765e4125b7b35d63db2
manideepla <manideepla@icloud.com>
2025-06-08 01:41:43 +0530

    feat: remove unwanted cors config

--- src/main/java/com/manideepla/bookerang/config/SecurityConfig.java
+++ src/main/java/com/manideepla/bookerang/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -31,9 +30,6 @@ public class SecurityConfig {
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true); // if using cookies or Authorization header
-
-                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                    source.registerCorsConfiguration("/**", config);
                     return config;
                 }))
                 .authorizeExchange(exchanges -> exchanges
