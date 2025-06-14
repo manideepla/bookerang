d368ddc5f33d70f723cbcac716079d0491ad0fc3
manideepla <manideepla@icloud.com>
2025-06-14 16:04:43 +0530

    feat: save or get author query

--- src/main/java/com/manideepla/bookerang/handlers/BookHandler.java
+++ src/main/java/com/manideepla/bookerang/handlers/BookHandler.java
@@ -1,12 +1,10 @@
 package com.manideepla.bookerang.handlers;
 
 import com.manideepla.bookerang.models.AddBookRequest;
-import com.manideepla.bookerang.models.Author;
 import com.manideepla.bookerang.models.Book;
 import com.manideepla.bookerang.models.UserCopy;
 import com.manideepla.bookerang.models.UserCopyItem;
 import com.manideepla.bookerang.models.NearbyBookItem;
-import com.manideepla.bookerang.models.GetBooksResponse;
 import com.manideepla.bookerang.repositories.AuthorRepository;
 import com.manideepla.bookerang.repositories.BookRepository;
 import com.manideepla.bookerang.repositories.CopyRepository;
@@ -38,7 +36,7 @@ public class BookHandler {
 
     public Mono<UUID> addBook(AddBookRequest addBookRequest, String username) {
         return Mono.just(addBookRequest.author())
-                .flatMap(authorName -> authorRepository.save(new Author(null, authorName)))
+                .flatMap(authorName -> authorRepository.saveOrGetAuthor(authorName))
                 .map(author -> new Book(addBookRequest.title(), author.id(), null))
                 .flatMap(book -> bookRepository.save(book))
                 .map(savedBook -> new UserCopy(null, savedBook.id(), username))
--- src/main/java/com/manideepla/bookerang/repositories/AuthorRepository.java
+++ src/main/java/com/manideepla/bookerang/repositories/AuthorRepository.java
@@ -1,9 +1,20 @@
 package com.manideepla.bookerang.repositories;
 
 import com.manideepla.bookerang.models.Author;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 public interface AuthorRepository extends ReactiveCrudRepository<Author, UUID> {
+
+    // Custom query for saving or getting author
+    @Query(value = """
+    INSERT INTO authors (name) VALUES (:authorName)
+    ON CONFLICT (name) DO UPDATE
+    SET name = authors.name
+    RETURNING id, name
+    """)
+    Mono<Author> saveOrGetAuthor(String authorName);
 }
